81f756476d6df0b0c3986ba147a7752a03f61cd4
ZRP <zrp1017@163.com>
2018-05-09 18:34:03 +0800

    ..

---
+++ myDS/vector.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+----------------------------------------
+File Name   :   Vector.py
+Description :   
+Author      :   Rongping Zhu (zrp1017@163.com)
+Date        :   2018-05-09
+Link        :   http://zrp15.top
+----------------------------------------
+Change Activity:
+                2018-05-09
+----------------------------------------
+"""
+
+__author__ = 'Rongping Zhu'
+
+
+class Vector(object):
+    """docstring for Vector"""
+    def __init__(self, arg):
+        super(Vector, self).__init__()
+        self.arg = arg
+        
